0f344c4508c06dfd19c9599e8910f1afc662ab0a
Terry Miller <millerterryt@gmail.com>
2022-12-07 10:38:51 -0500

    Removed code

--- database/database.py
+++ database/database.py
@@ -1,13 +1,1 @@
 import sqlite3
-
-connection = sqlite3.connect("linux_distro.db")
-
-cursor = connection.cursor()
-
-cursor.execute("create table linux (Distribution, " + ",".join(column_names)+ ")")
-for i in range(len(df)):
-	cursor.execute("insert into linux values (?,?,?,?,?,?,?,?,?,?,?,?)",
-	
-connection.commit()
-
-connection.close()
