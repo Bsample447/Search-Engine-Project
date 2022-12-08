792eaff7969e3f1790eeebc9dc2d59a17747ce67
Chris Light <lightusmc@gmail.com>
2022-12-08 09:34:47 -0500

    create_schema() method working correctly

--- modules/db.py
+++ modules/db.py
@@ -24,16 +24,27 @@ class Database():
 
     def create_table(self, table, columns):
         self.connect()
-        self.cur.execute(f"""create table {table} {columns}""")
+        self.cur.execute(f"create table {table}{columns}")
         self.close()
 
     def create_schema(self):
         self.connect()
         
-        if self.exists("Keywords") == False:
-            self.create_table("Keywords",
-                              """(keyword_id int primary key,
-                                 keyword varchar(64))""")
-        else:
-            print('Keywords table already exists')
+        _schema_list = [["Keywords", "(keyword_id int primary key, keyword varchar(64))"],
+                        ["URLs", "(url_id int primary key, url text, last_update datetime)"],
+                        ["Keywords_URLs", """(keyword_id int, url_id int,
+                                              foreign key(keyword_Id) references Keywords(keyword_id),
+                                              foreign key(url_id) references URLs(urls_id))"""],
+                        ["Blacklist", "(blacklist_id int primary key, blacklist varchar(64))"],
+                        ["Keywords_Blacklist", """(keyword_id int, blacklist_id int,
+                                                   foreign key(keyword_id) references Keyword(keyword_id),
+                                                   foreign key(blacklist_id) references Blacklist(blacklist_id))"""]]
+
+        for i in _schema_list:
+            if self.exists(i[0]) == False:
+                self.create_table(i[0], i[1])
+                print(f'{i[0]} has been created')
+            else:
+                print(f'{i[0]} table already exists')
+
         self.close()
--- neztest.py
+++ neztest.py
@@ -2,10 +2,4 @@ from modules import db
 
 if __name__ == "__main__":
     db = db.Database()
-    result = db.exists("Keywords")
-    print(result)
-
     db.create_schema()
-
-    result = db.exists("Keywords")
-    print(result)
