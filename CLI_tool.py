0422a608ae4a3467a58bbb1c767c8a1d1cf6a1b3
Terry Miller <millerterryt@gmail.com>
2022-12-14 15:23:00 -0500

    Takes user input and converts it into a list/lowers for db search.

--- CLI_tool.py
+++ CLI_tool.py
@@ -1,13 +1,15 @@
 import os
-from modules import db, crawler      #imports database and web crawler
-from threading import Thread
+from modules import db                  #imports database
+from crawler import *                   #imports everything from crawler.py
+from threading import Thread            #threading
+import time                             #time for threading
 
-new_thread = Thread(target=web_crawl()) #creates web_crawl as a thread
 
-new_thread.start()                      #starts web_crawl
 
-db = db.Database()                      #db assignment
 
+###############################################################################
+#                               Functions                                     #
+###############################################################################
 
 def clr():                              #clearing function 
     clr = os.system('clear')
@@ -17,8 +19,8 @@ def clr():                              #clearing function
 
 
 def user_menu():                        #first user menu
-    option = input("""
-What would you like to do?
+    clr()
+    option = input("""What would you like to do?
     1.) New Search
     2.) Exit
 """)
@@ -26,42 +28,48 @@ What would you like to do?
         clr()
         search_input()
     elif option == "2":
-        exit()
+        quit = str.lower(input("""Are you sure you want to quit?
+(Type 'yes' to quit)\n"""))
+        if quit == "yes":
+            exit()
+        else:
+            clr()
+            return user_menu()
     else:
-        input("""
-    Enter a number between 1 and 2.
+        clr()
+        input("""Enter a number between 1 and 2.
+Press any key to continue.
 """)
         clr()
         return user_menu()
 
 
 
-
-    
-            
-
-
-
-
 def search_input():                  #passes db.py user input keyword
     print("'This passes key word(s) to db.py'")
-    keyword = str(input(f"Type the key word(s) you would like to search for. \n")).lower()
+    search_input = str.lower(input(f"Type the key word(s) you would like to search for. \n"))
+    keyword = search_input.split()
     search_db(keyword)
 
-def search_db(keyword):
-    if keyword == "mountain":
+
+
+def search_db(keyword):             
+    if keyword == ['mountain']:
         print("you typed mountain")
     else:
-        run_crawler(keyword)
+        print(keyword)
+        return search_db
 
 
-def run_crawler(keyword):                  #will run the crawler with user input
-    print(keyword)
-
-
-
-
+###############################################################################
+#                               Main                                          #
+###############################################################################
 
 if __name__ == "__main__":
     user_menu()
+    new_thread = Thread(target=web_crawl)   #creates web_crawl
+    new_thread.daemon = True
+    new_thread.start()                      #starts web_crawl
+
+
 
