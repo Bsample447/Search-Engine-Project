167274340f6486443b725e86c0b2e9923ab5e91b
Terry Miller <millerterryt@gmail.com>
2022-12-14 11:47:46 -0500

    cleaned up

--- CLI_tool.py
+++ CLI_tool.py
@@ -1,11 +1,18 @@
 import os
-#from modules import db, web-crawler             #imports database and web crawler
+from modules import db, crawler      #imports database and web crawler
+from threading import Thread
+
+new_thread = Thread(target=web_crawl()) #creates web_crawl as a thread
+
+new_thread.start()                      #starts web_crawl
+
+db = db.Database()                      #db assignment
+
 
 def clr():                              #clearing function 
     clr = os.system('clear')
     return clr
 
-#db = db.Database()                      #db assignment
 
 
 
@@ -13,22 +20,16 @@ def user_menu():                        #first user menu
     option = input("""
 What would you like to do?
     1.) New Search
-    2.) Something Else
-    3.) Another Thing
-    4.) Exit
+    2.) Exit
 """)
     if option == "1":
         clr()
         search_input()
     elif option == "2":
-        pass
-    elif option == "3":
-        pass
-    elif option == "4":
         exit()
     else:
         input("""
-    Enter a number between 1 and 4.
+    Enter a number between 1 and 2.
 """)
         clr()
         return user_menu()
@@ -42,19 +43,17 @@ What would you like to do?
 
 
 
-def search_input():                  #searches db.py for a specific keyword
-    print("'This searches db.py for key word(s)'")
+def search_input():                  #passes db.py user input keyword
+    print("'This passes key word(s) to db.py'")
     keyword = str(input(f"Type the key word(s) you would like to search for. \n")).lower()
     search_db(keyword)
-    #    pass                        #if keyword matches what is in db, pull the db file.
-    #else:
-    #    run_crawler()               #if keyword has no matches, run the crawler for the keyword
 
 def search_db(keyword):
     if keyword == "mountain":
-        pass
+        print("you typed mountain")
     else:
         run_crawler(keyword)
+
 
 def run_crawler(keyword):                  #will run the crawler with user input
     print(keyword)
